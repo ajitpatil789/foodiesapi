1c6d77118f90b24b522c609441724ab95951bbed
Ajit Patil <ajitpatil789@gmail.com>
2025-07-21 15:02:36 +0530

    Health controller added for UptimeRobot

--- src/main/java/in/ajit/foodiesapi/config/SecurityConfig.java
+++ src/main/java/in/ajit/foodiesapi/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
 //                .cors(Customizer.withDefaults())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/register", "/api/login", "/api/foods/**", "/api/orders/all", "/api/orders/status/**").permitAll()
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/register", "/api/login", "/api/foods/**", "/api/orders/all", "/api/orders/status/**", "/health").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
